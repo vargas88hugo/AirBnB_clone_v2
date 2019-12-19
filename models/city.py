6c5c9941dab3228e73cd058cb56de6f299e79569
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-19 12:39:04 -0500

    [Feature] created and updated Place engine to store in the database

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """This is the city class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
@@ -19,3 +20,5 @@ class City(BaseModel, Base):
     state_id = Column(String(60),
                       ForeignKey('states.id'),
                       nullable=False)
+    places = relationship("Place", cascade="all, delete-orphan",
+                          backref="cities")
--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import backref
+from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
@@ -18,3 +19,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", cascade="all, delete-orphan",
+                          backref="user")
