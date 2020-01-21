4a1f7c4685bb73a4a2dba67b7db5b78fad74b3e5
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-21 13:39:39 -0500

    [Feature] Created 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application printing Hello World
+and include route /hbnb, also add a variable rule
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+# <> variable rules by marking section of the url
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C %s" % text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
