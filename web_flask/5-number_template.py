5576a58908fb940b0022bab4d0ad1b1d7d0c3cf5
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-21 14:15:37 -0500

    [Feature] Created 5-number_template.py with its template html

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application printing Hello World
+and include route /hbnb, also add a variable rule with c and python.
+
+
+"""
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+# <> variable rules by marking section of the url
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C %s" % text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is_cool'):
+    return "Python %s" % text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return "%d is a number" % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
