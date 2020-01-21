6aa4439f8a1e6e2bca8d45a211c533bba85a4cfa
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-21 12:26:13 -0500

    [Update] Changed sintaxis for applying strict flashes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,9 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_world():
     return 'Hello HBNB!'
 
